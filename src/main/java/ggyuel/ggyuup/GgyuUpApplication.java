0536b8169b4aa1fb34887de45ec3ab42851036d2
Lunabell03 <sunnism03@gmail.com>
2024-05-15 02:02:07 +0900

    test

---
+++ src/main/java/ggyuel/ggyuup/GgyuUpApplication.java
@@ -0,0 +1,13 @@
+package ggyuel.ggyuup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgyuUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgyuUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ggyuel/ggyuup/GgyuUpApplicationTests.java
@@ -0,0 +1,13 @@
+package ggyuel.ggyuup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgyuUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
